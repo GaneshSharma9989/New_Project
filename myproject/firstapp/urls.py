ea8eefd05b047db821e650c34f70e34f680bf82e
GaneshSharma9989 <152678557+GaneshSharma9989@users.noreply.github.com>
2025-05-12 14:52:19 +0530

    Update urls.py

--- myproject/firstapp/urls.py
+++ myproject/firstapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('skill', views.show_name_skills, name='show_name_skills'),
+    path('skill', views.show_name_skills, name='show_name_skillss'),
 ]
