a36846d629411da086c153f708ac7dd6f53c84da
ganesh17324 <ganesh17324@gmail.com>
2025-05-12 14:35:16 +0530

    i have updated in secondapp views.py

--- myproject/secondapp/views.py
+++ myproject/secondapp/views.py
@@ -8,4 +8,4 @@ def show_time(request):
     now = datetime.datetime.now()
     formatted_time = now.strftime("%Y-%B-%d, %H:%M") #to get year month day hour and sec
     #formatted_time=(now.strftime("%B")) to get only month specific
-    return HttpResponse(f"<h2>Hello World!</h2><p>Current Time: {formatted_time}</p>")
+    return HttpResponse(f"<h2>Hello World Time!</h2><p>Current Time: {formatted_time}</p>")
